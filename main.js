152da9a3a6b9b3ab022a4083aed00343ab34824c
Arcus286 <69086207+Arcus286@users.noreply.github.com>
2026-03-29 07:47:49 +0530

    Completed the project Finished the project and deployed it as well.

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn")
+const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
-if(leadsFromLocalStorage != null){
+if (leadsFromLocalStorage != null) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 function render(leads) {
   let listItems = "";
@@ -18,11 +27,11 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick",function (){
+deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
   render(myLeads);
-})
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
